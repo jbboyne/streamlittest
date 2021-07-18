32de1b85568c4617754c67488b1c25c1fa2a465d
Jeanette Boyne <jburries@gmail.com>
2021-07-18 15:20:18 -0700

    Update covidapp.py

--- covidapp.py
+++ covidapp.py
@@ -34,10 +34,10 @@ for country in countries:
     current_df = current_df.drop(columns = dropstats)
     current_df = current_df.drop(columns = ['NumDays', 'Country'])
     current_df = pd.melt(current_df, id_vars = ['Date'], value_vars = stats, var_name = 'Measure', value_name = 'Count')
-#     ts = current_df.resample('7D',on = 'Date')
+    ts = current_df.resample('7D',on = 'Date')
 #     current_df = pd.DataFrame(ts.mean())
     
-#     st.write(ts.mean())
+    st.write(ts.mean())
     
 #     line_chart = alt.Chart(current_df).mark_line().encode(
 #         x = 'Date',
