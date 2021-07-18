67841b8a5c0a22098d7c886d94c87232302b97f4
Jeanette Boyne <jburries@gmail.com>
2021-07-18 16:38:48 -0700

    Update covidapp.py

--- covidapp.py
+++ covidapp.py
@@ -32,7 +32,7 @@ df_subset = df_subset.resample('7D', on = 'Date').last()
 for country in countries:
     st.write(country)
     current_df = df_subset.loc[lambda d: d['Country'] == country]
-#     current_df = current_df.drop(columns = dropstats)
+    current_df = current_df.drop(columns = dropstats)
 #     current_df = current_df.drop(columns = ['NumDays', 'Country'])
 #     current_df = pd.melt(current_df, id_vars = ['Date', 'Country'], value_vars = stats, var_name = 'Measure', value_name = 'Count')
 # #     st.write(current_df)
