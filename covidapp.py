66f722af42a15608bd84c1c092ba2214d04b2776
Jeanette Boyne <jburries@gmail.com>
2021-07-18 16:44:44 -0700

    Update covidapp.py

--- covidapp.py
+++ covidapp.py
@@ -23,31 +23,31 @@ stats = st.sidebar.multiselect("Select stat", statlist)
 dropstats = statlist.drop(stats)
 
 df_subset = df.loc[lambda d: d['Country'].isin(countries)]
-df_dates = df_subset['Date']
-df_subset = df_subset.groupby(['Country'], as_index = False).rolling(window = 7).mean()
-df_subset = df_subset.join(df_dates)
-df_subset = df_subset.resample('7D', on = 'Date').last()
+# df_dates = df_subset['Date']
+# df_subset = df_subset.groupby(['Country'], as_index = False).rolling(window = 7).mean()
+# df_subset = df_subset.join(df_dates)
+# df_subset = df_subset.resample('7D', on = 'Date').last()
 st.write(df_subset['Country'].unique())
 
 
-for country in countries:
-    st.write(country)
-    st.write(df_subset)
-    st.write(df_subset['Country'].unique())
-    current_df = df_subset.loc[lambda d: d['Country'] == country]
-    current_df = current_df.drop(columns = dropstats)
-    current_df = current_df.drop(columns = ['NumDays', 'Country'])
-    current_df = pd.melt(current_df, id_vars = 'Date', value_vars = stats, var_name = 'Measure', value_name = 'Count')
-    st.write(current_df)
+# for country in countries:
+#     st.write(country)
+#     st.write(df_subset)
+#     st.write(df_subset['Country'].unique())
+#     current_df = df_subset.loc[lambda d: d['Country'] == country]
+#     current_df = current_df.drop(columns = dropstats)
+#     current_df = current_df.drop(columns = ['NumDays', 'Country'])
+#     current_df = pd.melt(current_df, id_vars = 'Date', value_vars = stats, var_name = 'Measure', value_name = 'Count')
+#     st.write(current_df)
     
-    line_chart = alt.Chart(current_df).mark_line().encode(
-        x = 'Date',
-        y = 'Count',
-        color='Measure',
-        strokeDash = 'Measure'
-    ).properties(
-        title='title'
-    )
+#     line_chart = alt.Chart(current_df).mark_line().encode(
+#         x = 'Date',
+#         y = 'Count',
+#         color='Measure',
+#         strokeDash = 'Measure'
+#     ).properties(
+#         title='title'
+#     )
 
-    st.altair_chart(line_chart)
+#     st.altair_chart(line_chart)
 
