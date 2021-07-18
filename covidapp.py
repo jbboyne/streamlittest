a101cdc36f18627843e18f2e69312eccc2278659
Jeanette Boyne <jburries@gmail.com>
2021-07-18 14:22:17 -0700

    Update covidapp.py

--- covidapp.py
+++ covidapp.py
@@ -9,8 +9,8 @@ df['NumDays'] = pd.to_datetime(df['Date']) - pd.to_datetime('2020-01-22')
 df['NumDays'] = pd.to_numeric(df['NumDays'])/(60*60*12*1000000000)
 df['NumDays'] = df['NumDays'].astype(int)
 
-df['Date'] = df['Date'].to_datetime
-# st.write(type(df['Date'][1]))
+pd.to_datetime(df['Date'])
+st.write(type(df['Date'][1]))
 
 st.title("COVID-19 Global Cases Time Series")
 
