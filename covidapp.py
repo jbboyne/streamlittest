8f4a64323babe122706fb21618869272601d927a
Jeanette Boyne <jburries@gmail.com>
2021-07-18 16:41:23 -0700

    Update covidapp.py

--- covidapp.py
+++ covidapp.py
@@ -27,15 +27,16 @@ df_dates = df_subset['Date']
 df_subset = df_subset.groupby(['Country'], as_index = False).rolling(window = 7).mean()
 df_subset = df_subset.join(df_dates)
 df_subset = df_subset.resample('7D', on = 'Date').last()
-# st.write(df_subset)
+
 
 for country in countries:
     st.write(country)
+    st.write(df_subset)
     current_df = df_subset.loc[lambda d: d['Country'] == country]
     current_df = current_df.drop(columns = dropstats)
     current_df = current_df.drop(columns = ['NumDays', 'Country'])
     current_df = pd.melt(current_df, id_vars = 'Date', value_vars = stats, var_name = 'Measure', value_name = 'Count')
-#     st.write(current_df)
+    st.write(current_df)
     
     line_chart = alt.Chart(current_df).mark_line().encode(
         x = 'Date',
