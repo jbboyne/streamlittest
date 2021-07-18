2d4dda37c5fc248ce8753f81bc2bcf1238f7dc29
Jeanette Boyne <jburries@gmail.com>
2021-07-18 16:32:05 -0700

    Update covidapp.py

--- covidapp.py
+++ covidapp.py
@@ -24,10 +24,12 @@ dropstats = statlist.drop(stats)
 
 df_subset = df.loc[lambda d: d['Country'].isin(countries)]
 df_dates = df_subset[['Date', 'Country']]
+st.write(df_subset)
 df_subset = df_subset.groupby(['Country']).rolling(window = 7).mean()
+st.write(df_subset)
 # df_subset = df_subset.join(df_dates, lsuffix = 'l', rsuffix = 'r')
 # df_subset = df_subset.resample('7D', on = 'Date').last()
-st.write(df_subset['Country'].unique())
+# st.write(df_subset['Country'].unique())
 
 # for country in countries:
 #     st.write(country)
