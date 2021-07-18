ffb06ad873f82aaba6a85edefdf4e8e1b7c56538
Jeanette Boyne <jburries@gmail.com>
2021-07-18 13:46:05 -0700

    Update covidapp.py

--- covidapp.py
+++ covidapp.py
@@ -31,7 +31,8 @@ for country in countries:
     current_df = df_subset.loc[lambda d: d['Country'] == country]
     current_df = current_df.drop(columns = dropstats)
     current_df = current_df.drop(columns = ['NumDays', 'Country'])
-    pd.melt(current_df, id_vars = ['Date'], value_vars = stats)
+    current_df = pd.melt(current_df, id_vars = ['Date'], value_vars = stats)
+    
     st.write(current_df.head())
 #     st.line_chart(current_df)
     
