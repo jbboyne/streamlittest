3e15bf3580758a9fd1198edd7a8146e255cd4e07
Jeanette Boyne <jburries@gmail.com>
2021-07-18 12:57:48 -0700

    Update covidapp.py

--- covidapp.py
+++ covidapp.py
@@ -5,9 +5,9 @@ import altair as alt
 url = "https://raw.githubusercontent.com/datasets/covid-19/main/data/countries-aggregated.csv"
 df = pd.read_csv(url)
 
-df['NumDays'] = pd.to_datetime(df['Date']) - pd.to_datetime('2020-01-22')
-df['NumDays'] = pd.to_numeric(df['NumDays'])/(60*60*12*1000000000)
-df['NumDays'] = df['NumDays'].astype(int)
+# df['NumDays'] = pd.to_datetime(df['Date']) - pd.to_datetime('2020-01-22')
+# df['NumDays'] = pd.to_numeric(df['NumDays'])/(60*60*12*1000000000)
+# df['NumDays'] = df['NumDays'].astype(int)
 
 st.title("COVID-19 Global Cases Time Series")
 
@@ -16,13 +16,13 @@ countries = st.sidebar.multiselect(
     df['Country'].unique()
     )
 
-numdays = st.sidebar.slider("Dates", min_value = min(df['NumDays']), max_value = max(df['NumDays']), step=1)
+# numdays = st.sidebar.slider("Dates", min_value = min(df['NumDays']), max_value = max(df['NumDays']), step=1)
 
 statlist = df.columns.drop(['Date', 'Country', 'NumDays'])
 stats = st.sidebar.multiselect("Select stat", statlist)
 
 df_subset = df.loc[lambda d: d['Country'].isin(countries)]
-df_subset = df_subset[df_subset['NumDays'] <= numdays]
+# df_subset = df_subset[df_subset['NumDays'] <= numdays]
 
 
 for country in countries:
