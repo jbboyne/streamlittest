0ca48a79048cfc4ec256447c295e631629202c73
Jeanette Boyne <jburries@gmail.com>
2021-07-18 16:38:31 -0700

    Update covidapp.py

--- covidapp.py
+++ covidapp.py
@@ -32,19 +32,19 @@ df_subset = df_subset.resample('7D', on = 'Date').last()
 for country in countries:
     st.write(country)
     current_df = df_subset.loc[lambda d: d['Country'] == country]
-    current_df = current_df.drop(columns = dropstats)
-    current_df = current_df.drop(columns = ['NumDays', 'Country'])
-    current_df = pd.melt(current_df, id_vars = ['Date', 'Country'], value_vars = stats, var_name = 'Measure', value_name = 'Count')
-#     st.write(current_df)
+#     current_df = current_df.drop(columns = dropstats)
+#     current_df = current_df.drop(columns = ['NumDays', 'Country'])
+#     current_df = pd.melt(current_df, id_vars = ['Date', 'Country'], value_vars = stats, var_name = 'Measure', value_name = 'Count')
+# #     st.write(current_df)
     
-    line_chart = alt.Chart(current_df).mark_line().encode(
-        x = 'Date',
-        y = 'Count',
-        color='Measure',
-        strokeDash = 'Measure'
-    ).properties(
-        title='title'
-    )
+#     line_chart = alt.Chart(current_df).mark_line().encode(
+#         x = 'Date',
+#         y = 'Count',
+#         color='Measure',
+#         strokeDash = 'Measure'
+#     ).properties(
+#         title='title'
+#     )
 
-    st.altair_chart(line_chart)
+#     st.altair_chart(line_chart)
 
