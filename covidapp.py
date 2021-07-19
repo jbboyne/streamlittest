502ac3b1d934d086aba980bf69d6034ee33fdf10
Jeanette Boyne <jburries@gmail.com>
2021-07-19 12:16:44 -0700

    Update covidapp.py

--- covidapp.py
+++ covidapp.py
@@ -48,9 +48,9 @@ type = st.sidebar.selectbox("Chart Type", ["Compare countries by each measure", 
 df_subset = df.loc[lambda d: d['Country'].isin(countries)]
 df_dates = df_subset['Date']
 df_subset = df_subset.groupby(['Country'], as_index = False).rolling(window = 7).mean()
-st.write(df_subset)
 df_subset = df_subset.join(df_dates)
 df_subset = df_subset.groupby(['Country'], as_index = False).resample('7D', on = 'Date').last()
+st.write(df_subset)
 
 if type == "Compare measures for each country":
     for country in countries:
