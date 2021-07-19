5150af97a8e388d60d333bdd2ffbe212c1e21be8
Jeanette Boyne <jburries@gmail.com>
2021-07-19 13:02:09 -0700

    Update covidapp.py

--- covidapp.py
+++ covidapp.py
@@ -6,21 +6,21 @@ import altair as alt
 url = "https://raw.githubusercontent.com/datasets/covid-19/main/data/countries-aggregated.csv"
 df = pd.read_csv(url)
 df = df.rename(columns={'Confirmed': 'Confirmed Cases'})
+
+#Get country population data
+url2 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQWDB7LlBd2xZivtvv4T_Wh7Bmqh79Ed6CWAZnyMB23-Q-yHpGGew9_0OLV2xWqVXDywBV07FFe7YhL/pub?gid=1075357968&single=true&output=csv"
+pop = pd.read_csv(url2)
+pop = pop.rename(columns={'Country (or dependency)': 'Country'})
+
+#dictionary for renaming population data to match covid country names
+dict = {'Myanmar': 'Burma', 'Côte d\'Ivoire':'Cote d\'Ivoire', 'South Korea': 'Korea, South',
+        'Saint Kitts & Nevis': 'Saint Kitts and Nevis', 'St. Vincent & Grenadines': 'Saint Vincent and the Grenadines',
+        'United States': 'US', 'Czech Republic (Czechia)': 'Czechia'}
+
+pop['Country'] = pop['Country'].replace(dict)
+
+df = pd.merge(df, pop[['Country', 'Population (2020)']], on='Country', how='left')
 st.write(df)
-
-# #Get country population data
-# url2 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQWDB7LlBd2xZivtvv4T_Wh7Bmqh79Ed6CWAZnyMB23-Q-yHpGGew9_0OLV2xWqVXDywBV07FFe7YhL/pub?gid=1075357968&single=true&output=csv"
-# pop = pd.read_csv(url2)
-# pop = pop.rename(columns={'Country (or dependency)': 'Country'})
-
-# #dictionary for renaming population data to match covid country names
-# dict = {'Myanmar': 'Burma', 'Côte d\'Ivoire':'Cote d\'Ivoire', 'South Korea': 'Korea, South',
-#         'Saint Kitts & Nevis': 'Saint Kitts and Nevis', 'St. Vincent & Grenadines': 'Saint Vincent and the Grenadines',
-#         'United States': 'US', 'Czech Republic (Czechia)': 'Czechia'}
-
-# pop['Country'] = pop['Country'].replace(dict)
-
-# df = pd.merge(df, pop[['Country', 'Population (2020)']], on='Country', how='left')
 
 # #Add a number of days count to each set of country data
 # df['NumDays'] = pd.to_datetime(df['Date']) - pd.to_datetime('2020-01-22')
