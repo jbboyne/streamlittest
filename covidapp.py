0a0aa835c8834812c656cfbfdeabb734f11dbd71
Jeanette Boyne <jburries@gmail.com>
2021-07-19 09:03:28 -0700

    Update covidapp.py

--- covidapp.py
+++ covidapp.py
@@ -5,6 +5,9 @@ import altair as alt
 url = "https://raw.githubusercontent.com/datasets/covid-19/main/data/countries-aggregated.csv"
 df = pd.read_csv(url)
 
+url2 = https://www.kaggle.com/tanuprabhu/population-by-country-2020?select=population_by_country_2020.csv
+pop = pd.read_csv(url2)
+
 df['NumDays'] = pd.to_datetime(df['Date']) - pd.to_datetime('2020-01-22')
 df['NumDays'] = pd.to_numeric(df['NumDays'])/(60*60*12*1000000000)
 df['NumDays'] = df['NumDays'].astype(int)
