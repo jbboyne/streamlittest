b7242978b4a53a0a30167d0ad3826ff28d6e6cb9
Jeanette Boyne <jburries@gmail.com>
2021-07-18 17:01:52 -0700

    Update covidapp.py

--- covidapp.py
+++ covidapp.py
@@ -26,10 +26,10 @@ dropstats = statlist.drop(stats)
 type = st.sidebar.selectbox("Chart Type", ["Compare countries by each measure", "Compare measures for each country"])
 
 df_subset = df.loc[lambda d: d['Country'].isin(countries)]
-df_dates = df_subset['Date']
-df_subset = df_subset.groupby(['Country'], as_index = False).rolling(window = 7).mean()
-df_subset = df_subset.join(df_dates)
-df_subset = df_subset.groupby(['Country'], as_index = False).resample('7D', on = 'Date').last()
+# df_dates = df_subset['Date']
+df_subset = df_subset.groupby([['Country', 'Date']], as_index = False).rolling(window = 7).mean()
+# df_subset = df_subset.join(df_dates)
+df_subset = df_subset.groupby([['Country', 'Date']], as_index = False).resample('7D', on = 'Date').last()
 # st.write(df_subset['Country'].unique())
 
 
