1ba8f205a0870d0e34cb09d512d51cc9ac58ee31
Jeanette Boyne <jburries@gmail.com>
2021-07-19 17:04:05 -0700

    Update covidapptest.py

--- covidapptest.py
+++ covidapptest.py
@@ -68,7 +68,7 @@ if type == "Compare measures for each country":
     for country in countries:
         st.write(country)
         current_df = df_subset.loc[lambda d: d['Country'] == country]
-#         popn = current_df['Population (2020)'].iloc[1]
+        popn = current_df['Population (2020)'].iloc[1]
 #         current_df = current_df.drop(columns = dropstats)
 #         current_df = current_df.drop(columns = ['NumDays', 'Country'])
 #         current_df = pd.melt(current_df, id_vars = ['Date'], value_vars = stats, var_name = 'Measure', value_name = 'Count')
