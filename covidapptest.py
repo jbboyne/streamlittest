20d218c7dbc612212d76e22ccd8c8414e783dc1d
Jeanette Boyne <jburries@gmail.com>
2021-07-19 17:04:32 -0700

    Update covidapptest.py

--- covidapptest.py
+++ covidapptest.py
@@ -70,7 +70,7 @@ if type == "Compare measures for each country":
         current_df = df_subset.loc[lambda d: d['Country'] == country]
         popn = current_df['Population (2020)'].iloc[1]
         current_df = current_df.drop(columns = dropstats)
-#         current_df = current_df.drop(columns = ['NumDays', 'Country'])
+        current_df = current_df.drop(columns = ['NumDays', 'Country'])
 #         current_df = pd.melt(current_df, id_vars = ['Date'], value_vars = stats, var_name = 'Measure', value_name = 'Count')
 #         current_df['Per Capita'] = (current_df['Count']/popn) * 100000
         
