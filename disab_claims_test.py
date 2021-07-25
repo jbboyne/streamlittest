17e89a4a6e0c21034ee8d47daff6387d41ba34b8
Jeanette Boyne <jburries@gmail.com>
2021-07-25 14:54:59 -0700

    Update disab_claims_test.py

--- disab_claims_test.py
+++ disab_claims_test.py
@@ -6,7 +6,6 @@ import altair as alt
 df_disab_count = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT7_v2esJv6ovgKoF8pMGT_A88Hm_nHOsfYcgOIaVqeUCv4KLJc2Zx2a9-8bq30EmbIqUCZHuPqasNh/pub?gid=2101361199&single=true&output=csv")
 df_disab_count['Year'] = df_disab_count['Year'].astype(str)
 df_disab_count['YOY change'] = df_disab_count.groupby(['State Code'])['All SSDI'].pct_change(1) * 100
-st.write(df_disab_count)
 
 #Create sidebar widgets
 states = st.sidebar.multiselect(
@@ -19,12 +18,12 @@ if states == []:
 
 df_subset = df_disab_count.loc[lambda d: d['State Code'].isin(states)]
 
-st.title("New disability claims by state")
+st.title("New disability claims by state, Year over Year Change")
 st.write("Choose different states with the widget in the left panel.")
 
 line_chart_all_SSDI_claims = alt.Chart(df_subset).mark_line().encode(
         x = 'Year',
-        y = 'All SSDI',
+        y = 'YOY change',
         color='State Code',
         strokeDash='State Code'
 )
