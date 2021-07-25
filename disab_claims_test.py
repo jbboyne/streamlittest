bcb305e9b40226ed4fb05adf281717c035f6e8e4
Jeanette Boyne <jburries@gmail.com>
2021-07-25 15:37:40 -0700

    Update disab_claims_test.py

--- disab_claims_test.py
+++ disab_claims_test.py
@@ -16,35 +16,35 @@ df_disab_count = df_disab_count.sort_values(by=['Year'], inplace=True)
 # st.write(df_recent_change)
 
 
-#Create sidebar widgets
-states = st.sidebar.multiselect(
-    "Select States",
-    df_disab_count['State Code'].unique()
-    )
+# #Create sidebar widgets
+# states = st.sidebar.multiselect(
+#     "Select States",
+#     df_disab_count['State Code'].unique()
+#     )
 
-# chgpct = st.sidebar.selectbox(
-#     "Select recent change range",
-#     df_disab_count
+# # chgpct = st.sidebar.selectbox(
+# #     "Select recent change range",
+# #     df_disab_count
 
-if states == []:
-        states = ['KY', 'TX', 'FL', 'GA']
+# if states == []:
+#         states = ['KY', 'TX', 'FL', 'GA']
 
-df_subset = df_disab_count.loc[lambda d: d['State Code'].isin(states)]
+# df_subset = df_disab_count.loc[lambda d: d['State Code'].isin(states)]
 
-st.title("New disability claims by state, Year over Year Change")
-st.write("Choose different states with the widget in the left panel.")
+# st.title("New disability claims by state, Year over Year Change")
+# st.write("Choose different states with the widget in the left panel.")
 
-line_chart_all_SSDI_claims = alt.Chart(df_subset).mark_line().encode(
-        x = 'Year',
-        y = 'YOY change',
-        color='State Code',
-        strokeDash='State Code'
-)
-
-st.altair_chart(line_chart_all_SSDI_claims)
-
-#
-# line_chart_SSDI_only = alt.Chart(df_disab_count).mark_line().encode(
+# line_chart_all_SSDI_claims = alt.Chart(df_subset).mark_line().encode(
 #         x = 'Year',
-#         y = '13 Receipts (Initial SSDI Only)')
-# st.altair_chart(line_chart_SSDI_only)
+#         y = 'YOY change',
+#         color='State Code',
+#         strokeDash='State Code'
+# )
+
+# st.altair_chart(line_chart_all_SSDI_claims)
+
+# #
+# # line_chart_SSDI_only = alt.Chart(df_disab_count).mark_line().encode(
+# #         x = 'Year',
+# #         y = '13 Receipts (Initial SSDI Only)')
+# # st.altair_chart(line_chart_SSDI_only)
