0a548bd947aac4119a10405842520050260a4c53
Jeanette Boyne <jburries@gmail.com>
2021-07-25 18:00:20 -0700

    Update disab_claims_test.py

--- disab_claims_test.py
+++ disab_claims_test.py
@@ -3,17 +3,15 @@ import pandas as pd
 import locale
 import altair as alt
 
-df_disab_count = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSTMpMSdP4qQl0yYntV-_X33w1J0AhoKHM_O4PMaBC7-RKeLJlH_lSOu7mvJm_seKU4hTsf-eBBB5pY/pub?gid=1080384046&single=true&output=csv")
+df_disab_count = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT7_v2esJv6ovgKoF8pMGT_A88Hm_nHOsfYcgOIaVqeUCv4KLJc2Zx2a9-8bq30EmbIqUCZHuPqasNh/pub?gid=2101361199&single=true&output=csv")
 df_disab_count = df_disab_count[~df_disab_count['State Code'].isin(['FE', 'GU', 'EA', 'EO', 'EM', 'EV'])]
 
-# df_disab_count['Year'] = df_disab_count['Year'].astype(str)
-df_disab_count['MOM change'] = df_disab_count.groupby(['State Code'])['All SSDI'].pct_change(1) * 100
+df_disab_count['Year'] = df_disab_count['Year'].astype(str)
+df_disab_count['YOY change'] = df_disab_count.groupby(['State Code'])['All SSDI'].pct_change(1) * 100
 
-df_disab_count['recent%'] =df_disab_count.groupby('State Code')['MOM change'].transform(lambda s: s.rolling(2, min_periods=1).mean())
-st.write(df_disab_count[df_disab_count['State Code'] == 'AK']) # and df_disab_count['Year'] >= 2000])
-changerates = df_disab_count[df_disab_count['Year'] == 2021][['State Code', 'recent%']]
+df_disab_count['recent%'] =df_disab_count.groupby('State Code')['YOY change'].transform(lambda s: s.rolling(2, min_periods=1).mean())
+changerates = df_disab_count[df_disab_count['Year'] == '2021'][['State Code', 'recent%']]
 changerates['recent%bin'] = pd.cut(changerates['recent%'], bins=5, precision=0, include_lowest=True, labels=["Lowest", "2", "3", "4", "Highest"]) 
-st.write(changerates.astype(str))
 
 
 #Create sidebar widgets
@@ -30,6 +28,10 @@ states = st.sidebar.multiselect(
     df_disab_count['State Code'].unique()
     )
 
+# values = ['<select>', "Lowest", "2", "3", "4", "Highest"]
+# default_ix = values.index("4")
+# window_ANTICOR = st.sidebar.selectbox('Window ANTICOR', values, index=default_ix)
+
 if chgpct == []:
     chgpct == '4'
 
@@ -41,27 +43,26 @@ if states == []:
         
 df_subset = df_disab_count.loc[lambda d: d['State Code'].isin(states)]
 
-st.title("New disability claims by state, Month over Month Change")
+st.title("New disability claims by state, Year over Year Change")
 st.write("Select quintile of average 2020-2021 change with the widget in the left panel.")
 st.write("Quintile: ", chgpct)
 line_chart_by_quintile = alt.Chart(df_subset2).mark_line().encode(
-        x = 'Year-Month',
-        y = 'MOM change',
+        x = 'Year',
+        y = 'YOY change',
         color='State Code',
         strokeDash='State Code'
 )
 
 st.altair_chart(line_chart_by_quintile)
 
-# st.title("New disability claims by state, Month over Month Change")
-# st.write("Choose different states with the widget in the left panel.")
+st.title("New disability claims by state, Year over Year Change")
+st.write("Choose different states with the widget in the left panel.")
 
-# line_chart_all_SSDI_claims = alt.Chart(df_subset).mark_line().encode(
-#         x = 'Month',
-#         y = 'YOY change',
-#         color='State Code',
-#         strokeDash='State Code'
-# )
+line_chart_all_SSDI_claims = alt.Chart(df_subset).mark_line().encode(
+        x = 'Year',
+        y = 'YOY change',
+        color='State Code',
+        strokeDash='State Code'
+)
 
-# st.altair_chart(line_chart_all_SSDI_claims)
-
+st.altair_chart(line_chart_all_SSDI_claims)
