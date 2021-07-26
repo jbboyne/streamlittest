94e407020213a90923c3bd664c401e1a1aedc320
Jeanette Boyne <jburries@gmail.com>
2021-07-25 19:17:05 -0700

    Update disab_claims_test.py

--- disab_claims_test.py
+++ disab_claims_test.py
@@ -11,7 +11,7 @@ df_disab_count['MOM change'] = df_disab_count.groupby(['State Code'])['All SSDI'
 avg_rate_by_year = df_disab_count.groupby(['Year', 'State Code'])['MOM change'].mean()
 st.write(avg_rate_by_year.index)
 avg_rate_by_year.index = avg_rate_by_year.index.set_names(['Year', 'State Code'])
-# avg_rate_by_year.merge(df_disab_count, how='right', right_on=['Year', 'State Code'])
+avg_rate_by_year.merge(df_disab_count, how='right', right_on=['Year', 'State Code'])
 # st.write(avg_rate_by_year)
 
 # df_disab_count['recent%'] =df_disab_count.groupby('State Code')['YOY change'].transform(lambda s: s.rolling(2, min_periods=1).mean())
