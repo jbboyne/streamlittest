503d676c827fb2d1ffda0e81490a8670f42283d1
Jeanette Boyne <jburries@gmail.com>
2021-07-25 18:55:51 -0700

    Update disab_claims_test.py

--- disab_claims_test.py
+++ disab_claims_test.py
@@ -9,64 +9,64 @@ st.write(df_disab_count)
 df_disab_count['Year'] = df_disab_count['Year'].astype(str)
 df_disab_count['MOM change'] = df_disab_count.groupby(['State Code'])['All SSDI'].pct_change(1) * 100
 avg_rate_by_year = df_disab_count.groupby(['Year', 'State Code'])['MOM change'].mean()
-avg_rate_by_year.reset_index(inplace=True)
+# avg_rate_by_year.reset_index(inplace=True)
 
-st.write(avg_rate_by_year)
+# st.write(avg_rate_by_year)
 
-df_disab_count['recent%'] =df_disab_count.groupby('State Code')['YOY change'].transform(lambda s: s.rolling(2, min_periods=1).mean())
-changerates = avg_rate_by_year[(avg_rate_by_year['Year'] == '2020') or (avg_rate_by_year['Year'] == '2021')][['State Code', 'recent%']]
-changerates['recent%bin'] = pd.cut(changerates['recent%'], bins=5, precision=0, include_lowest=True, labels=["Lowest", "2", "3", "4", "Highest"]) 
+# df_disab_count['recent%'] =df_disab_count.groupby('State Code')['YOY change'].transform(lambda s: s.rolling(2, min_periods=1).mean())
+# changerates = avg_rate_by_year[(avg_rate_by_year['Year'] == '2020') or (avg_rate_by_year['Year'] == '2021')][['State Code', 'recent%']]
+# changerates['recent%bin'] = pd.cut(changerates['recent%'], bins=5, precision=0, include_lowest=True, labels=["Lowest", "2", "3", "4", "Highest"]) 
 
 
-#Create sidebar widgets
-
-values = ['<select>', "Lowest", "2", "3", "4", "Highest"]
-default_ix = values.index("4")
-chgpct = st.sidebar.selectbox(
-    "Select 2020-2021 change rate quintile",
-    values, index=default_ix
-)
-
-states = st.sidebar.multiselect(
-    "Select States",
-    df_disab_count['State Code'].unique()
-    )
+# #Create sidebar widgets
 
 # values = ['<select>', "Lowest", "2", "3", "4", "Highest"]
 # default_ix = values.index("4")
-# window_ANTICOR = st.sidebar.selectbox('Window ANTICOR', values, index=default_ix)
+# chgpct = st.sidebar.selectbox(
+#     "Select 2020-2021 change rate quintile",
+#     values, index=default_ix
+# )
 
-if chgpct == []:
-    chgpct == '4'
+# states = st.sidebar.multiselect(
+#     "Select States",
+#     df_disab_count['State Code'].unique()
+#     )
 
-state_selection = changerates[changerates['recent%bin'] == chgpct]['State Code'].unique()
-df_subset2 = df_disab_count.loc[lambda d: d['State Code'].isin(state_selection)]
+# # values = ['<select>', "Lowest", "2", "3", "4", "Highest"]
+# # default_ix = values.index("4")
+# # window_ANTICOR = st.sidebar.selectbox('Window ANTICOR', values, index=default_ix)
 
-if states == []:
-        states = state_selection
+# if chgpct == []:
+#     chgpct == '4'
+
+# state_selection = changerates[changerates['recent%bin'] == chgpct]['State Code'].unique()
+# df_subset2 = df_disab_count.loc[lambda d: d['State Code'].isin(state_selection)]
+
+# if states == []:
+#         states = state_selection
         
-df_subset = df_disab_count.loc[lambda d: d['State Code'].isin(states)]
+# df_subset = df_disab_count.loc[lambda d: d['State Code'].isin(states)]
 
-st.title("New disability claims by state, Year over Year Change")
-st.write("Select quintile of average 2020-2021 change with the widget in the left panel.")
-st.write("Quintile: ", chgpct)
-line_chart_by_quintile = alt.Chart(df_subset2).mark_line().encode(
-        x = 'Year',
-        y = 'YOY change',
-        color='State Code',
-        strokeDash='State Code'
-)
+# st.title("New disability claims by state, Year over Year Change")
+# st.write("Select quintile of average 2020-2021 change with the widget in the left panel.")
+# st.write("Quintile: ", chgpct)
+# line_chart_by_quintile = alt.Chart(df_subset2).mark_line().encode(
+#         x = 'Year',
+#         y = 'YOY change',
+#         color='State Code',
+#         strokeDash='State Code'
+# )
 
-st.altair_chart(line_chart_by_quintile)
+# st.altair_chart(line_chart_by_quintile)
 
-st.title("New disability claims by state, Year over Year Change")
-st.write("Choose different states with the widget in the left panel.")
+# st.title("New disability claims by state, Year over Year Change")
+# st.write("Choose different states with the widget in the left panel.")
 
-line_chart_all_SSDI_claims = alt.Chart(df_subset).mark_line().encode(
-        x = 'Year',
-        y = 'YOY change',
-        color='State Code',
-        strokeDash='State Code'
-)
+# line_chart_all_SSDI_claims = alt.Chart(df_subset).mark_line().encode(
+#         x = 'Year',
+#         y = 'YOY change',
+#         color='State Code',
+#         strokeDash='State Code'
+# )
 
-st.altair_chart(line_chart_all_SSDI_claims)
+# st.altair_chart(line_chart_all_SSDI_claims)
