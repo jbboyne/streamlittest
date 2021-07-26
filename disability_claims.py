0e9f01755c2e751400ff28893df87dd9b6eb0928
Jeanette Boyne <jburries@gmail.com>
2021-07-26 15:24:33 -0700

    Update disability_claims.py

--- disability_claims.py
+++ disability_claims.py
@@ -35,7 +35,7 @@ changerates['bin'] = pd.cut(changerates['Avg change 2020-2021'], 5, labels=["Low
 
 #Create sidebar widgets
 
-values = ['<select>', "Lowest", "2nd", "3rd", "4th", "Highest""]
+values = ['<select>', "Lowest", "2nd", "3rd", "4th", "Highest"]
 default_ix = values.index("4")
 chgpct = st.sidebar.selectbox(
     "Select 2020-2021 change rate quintile",
