c44380965723e136225f9ddbef95e0d24e27b1ff
Jeanette Boyne <jburries@gmail.com>
2021-07-25 18:01:03 -0700

    Update disab_claims_test.py

--- disab_claims_test.py
+++ disab_claims_test.py
@@ -6,11 +6,11 @@ import altair as alt
 df_disab_count = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT7_v2esJv6ovgKoF8pMGT_A88Hm_nHOsfYcgOIaVqeUCv4KLJc2Zx2a9-8bq30EmbIqUCZHuPqasNh/pub?gid=2101361199&single=true&output=csv")
 df_disab_count = df_disab_count[~df_disab_count['State Code'].isin(['FE', 'GU', 'EA', 'EO', 'EM', 'EV'])]
 
-df_disab_count['Year'] = df_disab_count['Year'].astype(str)
+# df_disab_count['Year'] = df_disab_count['Year'].astype(str)
 df_disab_count['YOY change'] = df_disab_count.groupby(['State Code'])['All SSDI'].pct_change(1) * 100
 
 df_disab_count['recent%'] =df_disab_count.groupby('State Code')['YOY change'].transform(lambda s: s.rolling(2, min_periods=1).mean())
-changerates = df_disab_count[df_disab_count['Year'] == '2021'][['State Code', 'recent%']]
+changerates = df_disab_count[df_disab_count['Year'] == 2021][['State Code', 'recent%']]
 changerates['recent%bin'] = pd.cut(changerates['recent%'], bins=5, precision=0, include_lowest=True, labels=["Lowest", "2", "3", "4", "Highest"]) 
 
 
