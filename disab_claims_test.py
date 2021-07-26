83b38feb945cce6eb0a2cf990d15269d398d65d8
Jeanette Boyne <jburries@gmail.com>
2021-07-25 19:51:42 -0700

    Update disab_claims_test.py

--- disab_claims_test.py
+++ disab_claims_test.py
@@ -14,7 +14,7 @@ df_disab_count = avg_rate_by_year.merge(df_disab_count, how='right', on=['Year',
 df_disab_count = df_disab_count.rename(columns={'MOM change_x': 'Avg Monthly Change Per Year'})
 df_disab_count = df_disab_count.groupby(['Year', 'State Code']).mean()
 df_disab_count.index = df_disab_count.index.set_names(['Year', 'State Code'])
-# st.write(type(df_disab_count))
+st.write(type(df_disab_count))
 # df_disab_count = df_disab_count.reset_index(inplace=True)
 # st.write(type(df_disab_count)
 
