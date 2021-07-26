b3de4effe38fec2c874573f78e73860eafcf685d
Jeanette Boyne <jburries@gmail.com>
2021-07-25 17:15:07 -0700

    Update disab_claims_test.py

--- disab_claims_test.py
+++ disab_claims_test.py
@@ -6,10 +6,10 @@ import altair as alt
 df_disab_count = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT7_v2esJv6ovgKoF8pMGT_A88Hm_nHOsfYcgOIaVqeUCv4KLJc2Zx2a9-8bq30EmbIqUCZHuPqasNh/pub?gid=2101361199&single=true&output=csv")
 df_disab_count = df_disab_count[~df_disab_count['State Code'].isin(['FE', 'GU', 'EA', 'EO', 'EM', 'EV'])]
 
-df_disab_count['Year'] = df_disab_count['Year'].astype(str)
-df_disab_count['YOY change'] = df_disab_count.groupby(['State Code'])['All SSDI'].pct_change(1) * 100
+# df_disab_count['Year'] = df_disab_count['Year'].astype(str)
+df_disab_count['MOM change'] = df_disab_count.groupby(['State Code'])['All SSDI'].pct_change(1) * 100
 
-df_disab_count['recent%'] =df_disab_count.groupby('State Code')['YOY change'].transform(lambda s: s.rolling(2, min_periods=1).mean())
+df_disab_count['recent%'] =df_disab_count.groupby('State Code')['MOM change'].transform(lambda s: s.rolling(2, min_periods=1).mean())
 changerates = df_disab_count[df_disab_count['Year'] == '2021'][['State Code', 'recent%']]
 changerates['recent%bin'] = pd.cut(changerates['recent%'], bins=5, precision=0, include_lowest=True, labels=["Lowest", "2", "3", "4", "Highest"]) 
 
@@ -43,11 +43,11 @@ if states == []:
         
 df_subset = df_disab_count.loc[lambda d: d['State Code'].isin(states)]
 
-st.title("New disability claims by state, Year over Year Change")
+st.title("New disability claims by state, Month over Month Change")
 st.write("Select quintile of average 2020-2021 change with the widget in the left panel.")
 st.write("Quintile: ", chgpct)
 line_chart_by_quintile = alt.Chart(df_subset2).mark_line().encode(
-        x = 'Year',
+        x = 'Month',
         y = 'YOY change',
         color='State Code',
         strokeDash='State Code'
@@ -55,11 +55,11 @@ line_chart_by_quintile = alt.Chart(df_subset2).mark_line().encode(
 
 st.altair_chart(line_chart_by_quintile)
 
-st.title("New disability claims by state, Year over Year Change")
+st.title("New disability claims by state, Month over Month Change")
 st.write("Choose different states with the widget in the left panel.")
 
 line_chart_all_SSDI_claims = alt.Chart(df_subset).mark_line().encode(
-        x = 'Year',
+        x = 'Month',
         y = 'YOY change',
         color='State Code',
         strokeDash='State Code'
