bf4a4279a323a32f0f4cdad449560b7b1bc3094f
Jeanette Boyne <jburries@gmail.com>
2021-07-25 19:26:04 -0700

    Update disab_claims_test.py

--- disab_claims_test.py
+++ disab_claims_test.py
@@ -10,7 +10,8 @@ df_disab_count['Year'] = df_disab_count['Year'].astype(str)
 df_disab_count['MOM change'] = df_disab_count.groupby(['State Code'])['All SSDI'].pct_change(1) * 100
 avg_rate_by_year = df_disab_count.groupby(['Year', 'State Code'])['MOM change'].mean().to_frame()
 avg_rate_by_year.index = avg_rate_by_year.index.set_names(['Year', 'State Code'])
-avg_rate_by_year.merge(df_disab_count, how='right', on=['Year', 'State Code'])
+df_disab_count = avg_rate_by_year.merge(df_disab_count, how='right', on=['Year', 'State Code'])
+st.write(df_disab_count)
 
 # df_disab_count['recent%'] =df_disab_count.groupby('State Code')['YOY change'].transform(lambda s: s.rolling(2, min_periods=1).mean())
 # changerates = avg_rate_by_year[(avg_rate_by_year['Year'] == '2020') or (avg_rate_by_year['Year'] == '2021')][['State Code', 'recent%']]
