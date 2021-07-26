3b910e70dd17a2ea5b97c2f58cb063fdc57264f6
Jeanette Boyne <jburries@gmail.com>
2021-07-25 17:17:20 -0700

    Update disab_claims_test.py

--- disab_claims_test.py
+++ disab_claims_test.py
@@ -7,7 +7,7 @@ df_disab_count = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT
 df_disab_count = df_disab_count[~df_disab_count['State Code'].isin(['FE', 'GU', 'EA', 'EO', 'EM', 'EV'])]
 
 # # df_disab_count['Year'] = df_disab_count['Year'].astype(str)
-# df_disab_count['MOM change'] = df_disab_count.groupby(['State Code'])['All SSDI'].pct_change(1) * 100
+df_disab_count['MOM change'] = df_disab_count.groupby(['State Code'])['All SSDI'].pct_change(1) * 100
 
 # df_disab_count['recent%'] =df_disab_count.groupby('State Code')['MOM change'].transform(lambda s: s.rolling(2, min_periods=1).mean())
 # changerates = df_disab_count[df_disab_count['Year'] == '2021'][['State Code', 'recent%']]
