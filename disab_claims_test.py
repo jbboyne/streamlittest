20296bb8c2bcfdddc6877b6d4921c8b3c5559f99
Jeanette Boyne <jburries@gmail.com>
2021-07-25 19:38:42 -0700

    Update disab_claims_test.py

--- disab_claims_test.py
+++ disab_claims_test.py
@@ -6,17 +6,16 @@ import altair as alt
 df_disab_count = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSTMpMSdP4qQl0yYntV-_X33w1J0AhoKHM_O4PMaBC7-RKeLJlH_lSOu7mvJm_seKU4hTsf-eBBB5pY/pub?gid=1080384046&single=true&output=csv")
 df_disab_count = df_disab_count[~df_disab_count['State Code'].isin(['FE', 'GU', 'EA', 'EO', 'EM', 'EV'])]
 df_disab_count['Year'] = df_disab_count['Year'].astype(str)
+
 df_disab_count['MOM change'] = df_disab_count.groupby(['State Code'])['All SSDI'].pct_change(1) * 100
 avg_rate_by_year = df_disab_count.groupby(['Year', 'State Code'])['MOM change'].mean().to_frame()
 avg_rate_by_year.index = avg_rate_by_year.index.set_names(['Year', 'State Code'])
 df_disab_count = avg_rate_by_year.merge(df_disab_count, how='right', on=['Year', 'State Code'])
-st.write(df_disab_count.columns)
 df_disab_count = df_disab_count.rename(columns={'MOM change_x': 'Avg Monthly Change Per Year'})
 df_disab_count = df_disab_count.groupby(['Year', 'State Code']).mean()
-st.write(df_disab_count)
 
 # df_disab_count['recent%'] =df_disab_count.groupby('State Code')['YOY change'].transform(lambda s: s.rolling(2, min_periods=1).mean())
-changerates = avg_rate_by_year[(avg_rate_by_year['Year'] == '2020') or (avg_rate_by_year['Year'] == '2021')][['State Code', 'recent%']]
+# changerates = avg_rate_by_year[(avg_rate_by_year['Year'] == '2020') or (avg_rate_by_year['Year'] == '2021')][['State Code', 'recent%']]
 # changerates['recent%bin'] = pd.cut(changerates['recent%'], bins=5, precision=0, include_lowest=True, labels=["Lowest", "2", "3", "4", "Highest"]) 
 
 
